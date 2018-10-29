9f4c17aced7420b489164f953f2a4b7af1718278
leahchoi <soora913@gmail.com>
2018-10-29 14:10:23 -0700

    initial commit

---
+++ starter/app.js
@@ -0,0 +1,66 @@
+//Budget controller
+var budgetController = (function () {
+
+})();
+
+
+//UI controller
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
+
+    return {
+        getinput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+
+            }
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
+})();
+
+
+//Global app controller
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function(){
+        var DOM = UICtrl.DOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    
+
+    var ctrlAddItem = function () {
+        //1. Get the field input data
+        var input = UICtrl.getinput();
+        //2. Add the item to the budget controller
+        //3. Add the item to the UI
+        //4. Calculate the budget
+        //5. Display the budget on the UI
+    };
+    return {
+        init: function(){
+            console.log('app has started');
+            setupEventListeners();
+        }
+    };
+
+
+})(budgetController, UIController);
+
+controller.init();
