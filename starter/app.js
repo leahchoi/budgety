57facd1fb4831151098a7f5c44ad63942871ff18
leahchoi <soora913@gmail.com>
2018-10-29 16:20:39 -0700

    displays on the dom & clears the inputs

--- starter/app.js
+++ starter/app.js
@@ -91,6 +91,19 @@ var UIController = (function () {
             //Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + 
+            DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = '';
+            });
+            fieldsArr[0].focus();
+        },
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -121,6 +134,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         //2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
+        //4. Clear the fields
+        UICtrl.clearFields();
         //4. Calculate the budget
         //5. Display the budget on the UI
     };
